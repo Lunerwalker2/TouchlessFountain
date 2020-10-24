d05732513066a069a47c4acb46fecfaf5c15f082
Lunerwalker2 <55162022+Lunerwalker2@users.noreply.github.com>
2020-10-24 13:21:05 -0400

    feat(servo): Added the servo

--- touchless_fountain/touchless_fountain.ino
+++ touchless_fountain/touchless_fountain.ino
@@ -1,4 +1,6 @@
 
+#include <Servo.h>
+
 //Pins for the ultrasonic sensor
 const int trigPin = 9; //Ping pin
 const int echoPin = 10; //Echo pin
@@ -9,10 +11,31 @@ const float speedSoundPerMicrosec = 0.0343;
 float duration; //Elapsed time for each, in mircrosecs
 float distance; //Calculated distance to object in cm
 
+//Positions of the servo
+const int waterOnPos = 170;
+const int waterOffPos = 5;
+
+//Boolean that stores the current state of the servo
+boolean isWaterOn = false;
+
+//Distance threshold to turn on water
+const float distanceThreshold = 10; //e.g.  10 cm
+
+//Servo to control the knob
+Servo servo;
+
+
 void setup() {
   //Set up the pins used
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+
+  //Set up the servo on pin 6
+  servo.attach(6);
+
+  //Initially set up the servo to the off position
+  servo.write(waterOffPos);
+
   Serial.begin(9600);
 
 }
@@ -29,8 +52,18 @@ void loop() {
   //Distance in centimeters
   distance = calculateDistanceToObject();
 
+  if(distance <= distanceThreshold && !isWaterOn){
+    servo.write(waterOnPos);
+    isWaterOn = true;
+  } else if (distance > distanceThreshold && isWaterOn){
+    servo.write(waterOffPos);
+    isWaterOn = false;
+  }
+
+
   Serial.print("Distance (cm): ");
   Serial.println(distance);
+  Serial.println("Water On: " + isWaterOn);
   delay(100);
   
 }
